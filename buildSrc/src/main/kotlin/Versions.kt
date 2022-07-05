dfa9e9b02fe6e2f0612dc96be761882b3aab6c8a
collin.lee <collin.lee@kakaoenterprise.com>
2022-07-05 20:46:27 +0900

    trivial

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,9 +1,11 @@
 object Versions {
     private const val sdkCoreVersion = "0.0.2-SNAPSHOT"
     private const val sdkAppVersion = "0.0.2-SNAPSHOT"
+    private const val NONE = "NONE"
 
     val jarVersion = mapOf(
         "sdk-app" to sdkAppVersion,
         "sdk-core" to sdkCoreVersion,
+        NONE to NONE
     )
 }
